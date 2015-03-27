2856814507c76089449618b83f55e0d509544202
yaozhenfa <976691141@qq.com>
2015-03-27 17:25:40 +0800

    Create Project

---
+++ Sample/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Com.Umeng.Message;
+
+namespace Sample
+{
+    [Activity(Label = "Sample", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            PushAgent mPushAgent = PushAgent.GetInstance(this);
+            mPushAgent.Enable();
+
+            PushAgent.GetInstance(this).OnAppStart();
+        }
+    }
+}
+
